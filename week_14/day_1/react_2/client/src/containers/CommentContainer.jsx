448aa24bbbc5b9d88daf2f5c3ef4be70fdd0620d
FALLENau <reece.c.jones@gmail.com>
2017-08-21 15:04:10 +0100

    afternoon setup

---
+++ week_14/day_1/react_2/client/src/containers/CommentContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class CommentContainer extends React.Component {
+  constructor() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        Welcome to the Pain Train!
+      </div>
+    )
+  }
+}
+
+export default CommentContainer
